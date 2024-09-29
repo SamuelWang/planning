372584e1d08c5bf9b5fd335989ecccc646e27371
Samuel Wang <chieh0919@gmail.com>
2024-09-29 13:35:20 +0000

    use tailwindcss theme

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import Aura from '@primevue/themes/aura';
+// @ts-expect-error
+import Lara from '@/presets/tailwindcss-primevue-lara';
 import { createPinia } from 'pinia';
 import PrimeVue from 'primevue/config';
 import { createApp } from 'vue';
+import './assets/main.css';
 
 import App from './App.vue';
 import router from './router';
@@ -11,12 +13,8 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.use(PrimeVue, {
-  theme: {
-    preset: Aura,
-    options: {
-      darkModeSelector: '.app-theme-dark'
-    }
-  }
+  unstyled: true,
+  pt: Lara
 });
 
 app.mount('#app');
