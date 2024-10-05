e31200c1f44b30fe81c54b9296f3075f355d3358
Samuel Wang <chieh0919@gmail.com>
2024-10-05 13:39:20 +0000

    add TopAppBar component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+import TopAppBar from './components/layout/TopAppBar.vue';
 </script>
 
 <template>
+  <TopAppBar />
   <RouterView />
 </template>
---
+++ src/components/layout/TopAppBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="px-4 py-5">
+    <div class="flex content-center gap-4">
+      <img class="w-6 h-6" src="/src/assets/logo.png" />
+      <div class="font-normal text-[1.375rem] leading-7">Planning</div>
+      <div class="ml-auto">
+        <i class="pi pi-bars w-6 h-6 cursor-pointer"></i>
+      </div>
+    </div>
+  </header>
+</template>
